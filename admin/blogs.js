21e64e309c1259ab1e02e06df4a1357726a0ee3b
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-04-25 14:40:32 +0200

    changed api url to var

--- admin/blogs.js
+++ admin/blogs.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://derricks-brand.onrender.com';
+var API_URL = 'https://derricks-brand.onrender.com';
 // const API_URL = 'http://localhost:4000';
 // const cookie = document.cookie.split('jwt=')[1]
 
